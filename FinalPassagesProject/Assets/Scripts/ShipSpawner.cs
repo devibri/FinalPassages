95738e374f5af311c16b88f99d524eacd9ad881a
Devi Acharya <dacharya64@gmail.com>
2019-09-20 16:05:08 -0500

    working on different visualizations in js

---
+++ FinalPassagesProject/Assets/Scripts/ShipSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSpawner : MonoBehaviour
+{
+    public GameObject ship; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ship, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(ship, new Vector3(2, 0, 2), Quaternion.identity);
+        Instantiate(ship, new Vector3(-2, 0, 2), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
